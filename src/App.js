c76340441dfe4bd1cc3dff16bf7c47f8239a05cc
Leefo <abody88online@gamil.com>
2022-02-16 16:05:58 +0200

    Borwser basename => redux-toolkit-tut

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
@@ -10,12 +10,11 @@ import "./App.scss";
 function App() {
   return (
     <div className="app">
-      <Router>
-        <Header></Header>
+      <Router basename="/redux-toolkit-tut">
+        <Header />
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/redux-toolkit-tut" element={<Home />} />
             <Route path="/movie/:imdbID" element={<MovieDetail />} />
             <Route element={<PageNotFound />} />
           </Routes>
