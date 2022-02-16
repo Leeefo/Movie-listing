1287ce907f584ebc586b5b2e26381cdcb3df7341
Leefo <abody88online@gamil.com>
2022-02-16 16:51:03 +0200

    page not found

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,7 +1,21 @@
 import React from "react";
+import pnf from "../../images/pnf.jpg";
 
 const PageNotFound = () => {
-  return <div>PageNotFound</div>;
+  return <div
+    style={{
+      "textAlign": "center",
+      "height": "79vh",
+      "fontSize": "8vw",
+      "color": "white"
+    }}
+  >
+    <img src={pnf} style={{
+      "max-height": "79vh",
+      "margin": "0px",
+      "max-width": "100%"
+    }} />
+  </div>;
 };
 
 export default PageNotFound;
